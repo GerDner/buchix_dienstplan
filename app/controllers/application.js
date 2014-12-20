fabb132b8e2fb3e2135d5c007ebcd579c4e1fb98
Philipp Bucher <bucher@navigate.de>
2014-12-20 21:20:36 +0100

    Buttons

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
     itemController: 'item',
+    currentKw: function () {
+        return parseInt(moment().format('W'))
+    }.property(),
+    currentYear:  function(){
+        return parseInt(moment().format('GGGG'))
+    }.property(),
     actions: {
         login:function(){
             this.get('session').authenticate('authenticator:main', {name:this.get('loginName'),password:this.get('loginPassword')});
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+    
     saved: '',
     isLoading:false,
     /**

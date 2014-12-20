d2a813426cb687dd77c0ae71049c23ea5564fa05
Philipp Bucher <bucher@navigate.de>
2014-12-20 21:32:02 +0100

    Tooltip

--- app/views/tooltip.js
+++ app/views/tooltip.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.View.extend({
     template: Ember.Handlebars.compile('<span class="" > {{data-original-title}} </span>'),
     tagName: 'span',
-    classNames: ['tooltips','glyphicon'],
+    classNames: ['tooltips','glyphicon','pull-right'],
     attributeBindings: ['href', 'data-toggle', 'data-original-title'],
     classNameBindings: ['glyphicon'],
     href: '#',
